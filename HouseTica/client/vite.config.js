18fe3764375e0c3090500c3c020c32cca09e3a27
HarshMatolia <harry0987harrygg@gmail.com>
2024-10-10 01:30:24 +0530

    change proxy

--- HouseTica/client/vite.config.js
+++ HouseTica/client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://rental-website-housetica.onrender.com',
         secure: false,
       },
     },
